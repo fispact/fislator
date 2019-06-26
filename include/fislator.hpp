34ab449102e06ba19f6405d21873be8444a8b187
thomasms <stainer.tom@gmail.com>
2019-06-26 21:35:45 +0100

    Added command line argument parser

--- include/fislator.hpp
+++ include/fislator.hpp
@@ -13,32 +13,61 @@ using json = nlohmann::json;
 
 namespace fispact {
 
-    json from_json(std::string name){
-        std::ifstream ifs(name);
-        return json::parse(ifs);
-    }
-
-    void to_csv(json& j, std::string name){
-        std::ofstream ofs;
-        ofs.open(name);
-
-        std::vector<std::string> keys = { 
-            "irradiation_time", "cooling_time", "flux", "alpha_heat", "beta_heat", "gamma_heat"
-        };
-
-        std::copy(keys.begin(), keys.end()-1, std::ostream_iterator<std::string>(ofs, ","));
-        ofs << keys.back() <<"\n";
-
-        for (auto& element: j["inventory_data"]){
-            std::string delimiter = "";
-            for (auto& key: keys) {
-                ofs << delimiter << std::scientific << std::setprecision(15) << element[key].get<double>();
-                delimiter = ",";
-            }
-            ofs <<"\n";
+    namespace io {
+        
+        json from_json(std::string name){
+            std::ifstream ifs(name);
+            return json::parse(ifs);
         }
-        ofs.close();
-    }
+
+        void to_csv(json& j, std::string name){
+            std::ofstream ofs;
+            ofs.open(name);
+
+            std::vector<std::string> keys = { 
+                "irradiation_time", 
+                "cooling_time", 
+                "flux", 
+                "element", 
+                "isotope",
+                "state",
+                "grams", 
+                "activity",
+                "alpha_heat", 
+                "beta_heat", 
+                "gamma_heat",
+                "half_life",
+                "dose",
+                "ingestion",
+                "inhalation"
+            };
+
+            std::copy(keys.begin(), keys.end()-1, std::ostream_iterator<std::string>(ofs, ","));
+            ofs << keys.back() <<"\n";
+
+            for (auto& timestep: j["inventory_data"]){
+                for (auto& nuclide: timestep["nuclides"]){
+                    std::string delimiter = "";
+                    ofs << delimiter << std::scientific << std::setprecision(15) << timestep["irradiation_time"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << timestep["cooling_time"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << timestep["flux"].get<double>() << ","
+                        << delimiter << nuclide["element"].get<std::string>() << ","
+                        << delimiter << nuclide["isotope"].get<int>() << ","
+                        << delimiter << nuclide["state"].get<std::string>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["grams"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["activity"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["alpha_heat"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["beta_heat"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["gamma_heat"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["half_life"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["dose"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["ingestion"].get<double>() << ","
+                        << delimiter << std::scientific << std::setprecision(15) << nuclide["inhalation"].get<double>() << "\n";
+                }
+            }
+            ofs.close();
+        }
+    } // namespace io
 
     template <typename T>
     std::vector<T> inventoryvalues(const json& j, std::string key){
@@ -47,6 +76,7 @@ namespace fispact {
             values.emplace_back(element[key].get<T>());
         return values;
     }
-}
+
+} // namespace fispact
 
 #endif //FISLATER_HPP
--- tools/translate/main.cpp
+++ tools/translate/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <vector>
 
+#include <CLI/CLI.hpp>
+
 #include "fislator.hpp"
 
-int main(int argv, char** argc){
-    auto j = fispact::from_json("test31.json");
 
-    for(auto v: fispact::inventoryvalues<double>(j, "total_heat"))
-        std::cout << v << "\n";
+int main(int argc, char** argv){
+    CLI::App app{"FISPACT-II JSON translator"};
 
-    fispact::to_csv(j, "test31.csv");
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "The FISPACT-II JSON file");
+
+    CLI11_PARSE(app, argc, argv);
+
+    // read from JSON file
+    auto j = fispact::io::from_json(filename);
+    
+    // strip of extension
+    size_t lastindex = filename.find_last_of("."); 
+    std::string rawname = filename.substr(0, lastindex); 
+
+    // to csv
+    fispact::io::to_csv(j, rawname + ".csv");
+
     return 0;
 }
