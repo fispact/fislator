5bd3a19b79eaccb6b6bea8874cfb670f49e68234
thomasms <stainer.tom@gmail.com>
2019-06-28 21:28:03 +0100

    Refactoring and improvements

--- include/fislator.hpp
+++ include/fislator.hpp
@@ -11,16 +11,19 @@
 
 using json = nlohmann::json;
 
-namespace fispact {
+namespace fislator {
 
-    namespace io {
-        
-        json from_json(std::string name){
-            std::ifstream ifs(name);
-            return json::parse(ifs);
-        }
+    // process from JSON file from FISPACT-II
+    json from_json(std::string name){
+        std::ifstream ifs(name);
+        return json::parse(ifs);
+    }
 
-        void to_csv(json& j, std::string name){
+    // inventory operations
+    namespace inventory { 
+
+        template<char delimiter>
+        void to_sep_file(const json& j, std::string name){
             std::ofstream ofs;
             ofs.open(name);
 
@@ -42,41 +45,46 @@ namespace fispact {
                 "inhalation"
             };
 
-            std::copy(keys.begin(), keys.end()-1, std::ostream_iterator<std::string>(ofs, ","));
+            // std::copy(keys.begin(), keys.end()-1, std::ostream_iterator<std::string>(ofs, delimiter));
+            for(auto it=keys.begin(); it<keys.end()-1;++it)
+                ofs << *it << delimiter;
             ofs << keys.back() <<"\n";
 
             for (auto& timestep: j["inventory_data"]){
                 for (auto& nuclide: timestep["nuclides"]){
-                    std::string delimiter = "";
-                    ofs << delimiter << std::scientific << std::setprecision(15) << timestep["irradiation_time"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << timestep["cooling_time"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << timestep["flux"].get<double>() << ","
-                        << delimiter << nuclide["element"].get<std::string>() << ","
-                        << delimiter << nuclide["isotope"].get<int>() << ","
-                        << delimiter << nuclide["state"].get<std::string>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["grams"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["activity"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["alpha_heat"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["beta_heat"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["gamma_heat"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["half_life"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["dose"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["ingestion"].get<double>() << ","
-                        << delimiter << std::scientific << std::setprecision(15) << nuclide["inhalation"].get<double>() << "\n";
+                    ofs << std::scientific << std::setprecision(15) << timestep["irradiation_time"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << timestep["cooling_time"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << timestep["flux"].get<double>() << delimiter
+                        << nuclide["element"].get<std::string>() << delimiter
+                        << nuclide["isotope"].get<int>() << delimiter
+                        << nuclide["state"].get<std::string>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["grams"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["activity"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["alpha_heat"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["beta_heat"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["gamma_heat"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["half_life"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["dose"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["ingestion"].get<double>() << delimiter
+                        << std::scientific << std::setprecision(15) << nuclide["inhalation"].get<double>() << "\n";
                 }
             }
             ofs.close();
         }
-    } // namespace io
 
-    template <typename T>
-    std::vector<T> inventoryvalues(const json& j, std::string key){
-        std::vector<T> values;
-        for (auto& element: j["inventory_data"])
-            values.emplace_back(element[key].get<T>());
-        return values;
-    }
+        const auto to_csv = to_sep_file<','>;
+        const auto to_tab = to_sep_file<' '>;
 
-} // namespace fispact
+        template <typename T>
+        std::vector<T> values(const json& j, std::string key){
+            std::vector<T> values;
+            for (auto& element: j["inventory_data"])
+                values.emplace_back(element[key].get<T>());
+            return values;
+        }
+
+    }// namespace inventory
+
+} // namespace fislator
 
 #endif //FISLATER_HPP
--- tools/translate/main.cpp
+++ tools/translate/main.cpp
@@ -6,28 +6,41 @@
 #include "fislator.hpp"
 
 
+struct CLIOption{
+    std::string name;
+    bool use;
+    std::function<void(const json&, std::string)> writefunc;
+};
+
 int main(int argc, char** argv){
     CLI::App app{"FISPACT-II JSON translator"};
 
     std::string filename = "default";
-    bool usecsv = false;
+    std::vector<CLIOption> options = {
+        { "csv", false, fislator::inventory::to_csv },
+        { "tab", false, fislator::inventory::to_tab }
+    };
     app.add_option("-f,--file", filename, "The FISPACT-II JSON file")
         ->required()
         ->check(CLI::ExistingFile);
-    app.add_flag("--csv", usecsv, "Option to write csv");
+        
+    for (auto& option : options)
+        app.add_flag("--" + option.name, option.use, "Option to write " + option.name + " file");
 
     CLI11_PARSE(app, argc, argv);
 
     // read from JSON file
-    auto j = fispact::io::from_json(filename);
+    auto j = fislator::from_json(filename);
     
     // strip of extension
     size_t lastindex = filename.find_last_of("."); 
     std::string rawname = filename.substr(0, lastindex); 
 
-    // to csv
-    if(usecsv)
-        fispact::io::to_csv(j, rawname + ".csv");
+    // file options
+    for (auto const& option : options) {
+        if(option.use)
+            option.writefunc(j, rawname + "." + option.name);
+    }
 
     return 0;
 }
