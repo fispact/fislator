e9f73c5612877429430cc92549313db21f64641a
thomasms <stainer.tom@gmail.com>
2021-03-21 13:26:48 +0100

    Tool not actually catching exceptions, improve error handling a bit

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -5,100 +5,59 @@
 #include <exception>
 #include <string>
 
-
 namespace fislator {
 
-    /**
-     *   @brief  Base level exception FislatorException
-     *
-     *           Should be extended with subclasses
-     */
-    class FislatorException : public std::exception
-    {
-    public:
-        FislatorException(std::string text) : _what(text)
-        {
-        }
-        
-        virtual ~FislatorException() throw()
-        {
-        }
-        
-        virtual const char* what() const throw()
-        {
-            return std::string("FislatorException: " + _what).c_str();
-        }
-        
-    protected:
-        std::string _what;
-    };
+/**
+ *   @brief  Base level exception FislatorException
+ *
+ *           Should be extended with subclasses
+ */
+class FislatorException : public std::exception {
+public:
+  FislatorException(std::string text) : _what(text) {}
+  virtual ~FislatorException() throw() {}
+  virtual const char *what() const throw() { return _what.c_str(); }
 
-    /**
-     *   @brief  Invalid JSON file exception InvalidJSONFileException
-     *
-     *           Throw when issues when JSON file is not from F-II.
-     */
-    class InvalidJSONFileException : public FislatorException
-    {
-    public:
-        InvalidJSONFileException(std::string text) : FislatorException(text)
-        {
-        }
-        
-        virtual ~InvalidJSONFileException() throw()
-        {
-        }
-        
-        virtual const char* what() const throw()
-        {
-            return std::string("InvalidJSONFileException: " + _what).c_str();
-        }
-    };
+protected:
+  std::string _what;
+};
 
-    /**
-     *   @brief  Key lookup exception JSONKeyNotFoundException
-     *
-     *           Throw when issues when cannot find a key in the JSON file.
-     */
-    class JSONKeyNotFoundException : public FislatorException
-    {
-    public:
-        JSONKeyNotFoundException(std::string text) : FislatorException(text)
-        {
-        }
-        
-        virtual ~JSONKeyNotFoundException() throw()
-        {
-        }
-        
-        virtual const char* what() const throw()
-        {
-            return std::string("JSONKeyNotFoundException: " + _what).c_str();
-        }
-    };
+/**
+ *   @brief  Invalid JSON file exception InvalidJSONFileException
+ *
+ *           Throw when issues when JSON file is not from F-II.
+ */
+class InvalidJSONFileException : public FislatorException {
+public:
+  InvalidJSONFileException(std::string text) : FislatorException(text) {}
 
-    /**
-     *   @brief  File Format exception FileFormatReadException
-     *
-     *           Throw when issues with file formatting (reading/writing)
-     */
-    class FileFormatReadException : public FislatorException
-    {
-    public:
-        FileFormatReadException(std::string text) : FislatorException(text)
-        {
-        }
-        
-        virtual ~FileFormatReadException() throw()
-        {
-        }
-        
-        virtual const char* what() const throw()
-        {
-            return std::string("FileFormatReadException: " + _what).c_str();
-        }
-    };
+  virtual ~InvalidJSONFileException() throw() {}
+};
 
-} //fislator
+/**
+ *   @brief  Key lookup exception JSONKeyNotFoundException
+ *
+ *           Throw when issues when cannot find a key in the JSON file.
+ */
+class JSONKeyNotFoundException : public FislatorException {
+public:
+  JSONKeyNotFoundException(std::string text) : FislatorException(text) {}
+
+  virtual ~JSONKeyNotFoundException() throw() {}
+};
+
+/**
+ *   @brief  File Format exception FileFormatReadException
+ *
+ *           Throw when issues with file formatting (reading/writing)
+ */
+class FileFormatReadException : public FislatorException {
+public:
+  FileFormatReadException(std::string text) : FislatorException(text) {}
+
+  virtual ~FileFormatReadException() throw() {}
+};
+
+} // namespace fislator
 
 #endif // EXCEPTIONS_HPP
--- tools/translate/main.cpp
+++ tools/translate/main.cpp
@@ -5,42 +5,56 @@
 
 #include "fislator.hpp"
 
-
-struct CLIOption{
-    std::string name;
-    bool use;
-    std::function<void(const json&, std::string)> writefunc;
+struct CLIOption {
+  std::string name;
+  bool use;
+  std::function<void(const json &, std::string)> writefunc;
 };
 
-int main(int argc, char** argv){
-    CLI::App app{"FISPACT-II JSON translator"};
+int main(int argc, char **argv) {
+  CLI::App app{"FISPACT-II JSON translator"};
 
-    std::string filename = "default";
-    std::vector<CLIOption> options = {
-        { "csv", false, fislator::inventory::to_csv },
-        { "tab", false, fislator::inventory::to_tab }
-    };
-    app.add_option("-f,--file", filename, "The FISPACT-II JSON file")
-        ->required()
-        ->check(CLI::ExistingFile);
-        
-    for (auto& option : options)
-        app.add_flag("--" + option.name, option.use, "Option to write " + option.name + " file");
+  std::string filename = "default";
+  std::vector<CLIOption> options = {
+      {"csv", false, fislator::inventory::to_csv},
+      {"tab", false, fislator::inventory::to_tab}};
+  app.add_option("-f,--file", filename, "The FISPACT-II JSON file")
+      ->required()
+      ->check(CLI::ExistingFile);
 
-    CLI11_PARSE(app, argc, argv);
+  for (auto &option : options)
+    app.add_flag("--" + option.name, option.use,
+                 "Option to write " + option.name + " file");
 
+  CLI11_PARSE(app, argc, argv);
+
+  try {
     // read from JSON file
-    auto j = fislator::from_json(filename);
-    
+    const auto j = fislator::from_json(filename);
+
     // strip of extension
-    size_t lastindex = filename.find_last_of("."); 
-    std::string rawname = filename.substr(0, lastindex); 
+    const size_t lastindex = filename.find_last_of(".");
+    const std::string rawname = filename.substr(0, lastindex);
 
     // file options
-    for (auto const& option : options) {
-        if(option.use)
-            option.writefunc(j, rawname + "." + option.name);
+    for (auto const &option : options) {
+      if (option.use)
+        option.writefunc(j, rawname + "." + option.name);
     }
+  } catch (const fislator::InvalidJSONFileException &ex) {
+    std::cout << "InvalidJSONFileException::" << ex.what() << "\n";
+    return 1;
+  } catch (const fislator::JSONKeyNotFoundException &ex) {
+    std::cout << "JSONKeyNotFoundException::" << ex.what() << "\n";
+    return 2;
+  } catch (const fislator::FileFormatReadException &ex) {
+    std::cout << "FileFormatReadException::" << ex.what() << "\n";
+    return 3;
+  } catch (...) {
+    std::cout << "Unknown error!"
+              << "\n";
+    return 999;
+  }
 
-    return 0;
+  return 0;
 }
